90de897b5e6a48cc5beaaedfe2edb5f6e4b96f84
HaniaSalah <mlStudent978@gmail.com>
2024-12-28 16:12:06 +0200

    calculator

---
+++ main.py
@@ -0,0 +1,16 @@
+first_number = int(input())
+second_number = int(input())
+
+subtraction = 0
+
+sum = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+
+print("First number = ", first_number)
+print("Second number", second_number)
+print("Sum = ", sum)
+print("Subtraction = ", subtraction)
+print("Multiplication = ", multiplication)
+print("Division = ", division)
